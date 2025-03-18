71c2e930d30b00082499349f7b9c1de84fdb43a8
jerehut <jere@vaihe.com>
2025-03-18 13:52:24 +0200

    feat: added GetTickets and Footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap()]
+  integrations: [sitemap()],
+  site: "https://paris2025.gosim.org/",
 });
